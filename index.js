1d4f24c442b473e9dd59c82048eab0d2a9209e41
ajimix <ajimix@gmail.com>
2017-08-24 12:02:25 +0200

    Adds configuration file

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const fs = require('fs');
+
+if (!fs.existsSync('./config.json')) {
+  return console.error('[ERROR] Please create config.json file first');
+}
+
 const socks = require('socksv5');
-const port = 1080;
+const config = require('./config.json');
+const configPort = config.port || 1080;
+const configUser = config.user || 'nodejs';
+const configPass = config.pass || 'works';
 
 const srv = socks.createServer((info, accept, deny) => {
   accept();
 });
 
-srv.listen(port, function() {
-  console.log('SOCKS server listening on port ' + port);
+srv.listen(configPort, function() {
+  console.log('SOCKS server listening on port: ' + configPort + ' user: ' + configUser + ' pass: ' + configPass);
 });
 
 srv.useAuth(socks.auth.UserPassword((user, password, cb) => {
-  cb(user === 'nodejs' && password === 'works');
+  cb(user === configUser && password === configPass);
 }));
